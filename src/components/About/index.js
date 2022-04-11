0c64bffcc8a8e1a0299273e32fc5e42ee6fe1d0a
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2022-04-10 21:47:44 -0400

    content for about me section added and displayed

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,9 +3,40 @@ import selfImg from '../../assets/images/Zacharycampanelli.jpg';
 
 function About() {
   return (
-    <section className="my-5">
-      <h1 id="about">Zack</h1>
-      <img src={selfImg} className="my-2  d-flex" style={{ width: '100%' }} alt="developer" />
+    <section id="about_me" className="bio">
+      <div className="container-fluid my-bio">
+        <div className="row">
+          <div className="col-12 col-lg-6 text-center">
+            <img
+              src={selfImg}
+              alt="Zachary Campanelli"
+              className="img-fluid aniview-v4"
+              //   data-av-animation="animate__fadeInLeft"
+            />
+          </div>
+          <div className="col-12 col-lg-6">
+            <h3 className="section_title">About Me</h3>
+            <p>Full-Stack Developer, Graphic Designer, Life-Long Student and avid coffee drinker ☕</p>
+            <br />
+            <p>
+              Front-end web developer currently learning the back-end side of development. Attended Central Connecticut
+              State University with a Bachelor’s degree in Graphic Design and a minor in Computer Science, as well as
+              earning a Certificate in Full-Stack web development through the University of Connecticut. Strong HTML,
+              CSS, and JavaScript with newly acquired skills in responsive web design, Node.js, and APIs. Exceedingly
+              strong work ethic and passionate about learning new things. My interests lie in mobile-first design and
+              mobile app development. My dedication to my work and eagerness to learn new things make me an important
+              asset on any team.
+            </p>
+            <br />
+            <p>
+              <a href="./assets/images/Campanelli_resume.pdf" download>
+                {' '}
+                Download my resume here
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
