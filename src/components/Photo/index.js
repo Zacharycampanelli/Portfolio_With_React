9467517e60b0921f197e64ed5b64c436b9f8b846
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2022-04-11 22:06:47 -0400

    images and image data added and images are displayed on page

--- src/components/Photo/index.js
+++ src/components/Photo/index.js
@@ -1,11 +1,6 @@
-import React, { useState } from 'react'
-
-
-
-
+import React, { useState } from 'react';
 
 function Photo() {
-
   const [photos] = useState([
     {
       name: 'Spelling Practice',
@@ -14,9 +9,7 @@ function Photo() {
         'Spelling Practice is an app that can help anyone improve their spelling. This application uses separate API calls to display the description of a word, and speak the word out loud, leaving the user to take their best guess at spelling that particular word',
       github: 'https://github.com/Zacharycampanelli/SpellingPractice',
       deployed: 'https://zacharycampanelli.github.io/SpellingPractice/',
-      file: 'spellingpractice'
-
-
+      file: 'spellingpractice',
     },
     {
       name: 'Weather Dashboard',
@@ -25,8 +18,7 @@ function Photo() {
         'A weather dashboard page that uses the OpenWeatherMap API. The user searches for a city and is presented with both the current day forecast as well as the next five days.',
       github: 'https://github.com/Zacharycampanelli/Weather_Dashboard',
       deployed: 'https://zacharycampanelli.github.io/Weather_Dashboard/',
-      file: 'weather'
-
+      file: 'weather',
     },
     {
       name: 'Personal Info Locker',
@@ -35,8 +27,7 @@ function Photo() {
         'An application that holds your personal login information for each and every website you need. This project was built using Express.js, Sequelize, and Handlebars',
       github: 'https://github.com/Zacharycampanelli/Personal_Info_Locker',
       deployed: 'https://sleepy-cove-57890.herokuapp.com/',
-      file: 'personalinfolocker'
-
+      file: 'personalinfolocker',
     },
     {
       name: 'Password Generator',
@@ -45,8 +36,7 @@ function Photo() {
         'Creates a string of characters following the conditions the User asks for to create a truly random password',
       github: 'https://github.com/Zacharycampanelli/Password_Generator',
       deployed: 'https://zacharycampanelli.github.io/Password_Generator/',
-      file: 'password'
-
+      file: 'password',
     },
     {
       name: 'Taskmaster Pro',
@@ -55,38 +45,31 @@ function Photo() {
         'Taskmaster Pro is a Kanban style application that allows the user to track and categorize tasks based off of their progress. The data is saved in local storage and the application makes use of multiple third-party APIs including bootstrap, jQuery, jQuery UI, and Moment.js',
       github: 'https://github.com/Zacharycampanelli/Taskmaster_Pro',
       deployed: 'https://zacharycampanelli.github.io/Taskmaster_Pro/',
-      file: 'taskmaster'
-
+      file: 'taskmaster',
     },
     {
       name: 'Solar Bears',
       tools: 'HTML | CSS',
-      description:
-        'A website for an imaginary Solar Panel Company',
+      description: 'A website for an imaginary Solar Panel Company',
       github: 'https://github.com/Zacharycampanelli/Solar_Bears',
       deployed: 'https://zacharycampanelli.github.io/Solar_Bears/',
-      file: 'solarbears'
-
-    }
-   
+      file: 'solarbears',
+    },
   ]);
 
-
-
   return (
-    <div className='flex-row'>
-    {photos.map((image) => (
-      <img 
-      src={require(`../../assets/images/${image.file}.png`)}
-      alt={image.name}
-      className='img-thumbnail col-12 col-md-4'
-      // onClick={}
-      key={image.name}
-      />
-
-    ))}
-</div>
-  )
+    <div className="flex-row">
+      {photos.map((image) => (
+        <img
+          src={require(`../../assets/images/${image.file}.png`)}
+          alt={image.name}
+          className="img-thumbnail col-12 col-md-4"
+          // onClick={}
+          key={image.name}
+        />
+      ))}
+    </div>
+  );
 }
 
-export default Photo
+export default Photo;
