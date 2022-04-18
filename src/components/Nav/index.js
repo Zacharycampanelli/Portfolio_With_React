31f834626b8022b84b86f0a7d74248b08010e1cb
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2022-04-17 21:00:33 -0400

    clean up css file

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -69,60 +69,7 @@ function Nav(props) {
 
 
 
-  // return (
-  //   <header className="banner">
-  //     <nav className="navbar navbar-expand-md navbar-dark">
-  //       <div className="container-fluid">
-  //         <a className="navbar-brand" id="navbar-title" href="#">
-  //           Zachary Campanelli
-  //         </a>
-  //         <button
-  //           className="navbar-toggler"
-  //           type="button"
-  //           data-toggle="collapse"
-  //           data-target="#collapseNavbar"
-  //           aria-controls="navbarsExample01"
-  //           aria-expanded="false"
-  //           aria-label="Toggle navigation"
-  //         >
-  //           <span className="navbar-toggler-icon"></span>
-  //         </button>
-  //         <div className="collapse navbar-collapse justify-content-end" id="collapseNavbar">
-  //           <ul className="navbar-nav">
-            
-//               <li className="nav-item">
-//                 <a className="nav-link" href="#about_me">
-//                   About Me
-//                 </a>
-//               </li>
-//               <li className="nav-item">
-//                 <a className="nav-link" href="#my_work">
-//                   My Work
-//                 </a>
-//               </li>
-//               <li className="nav-item">
-//                 <a className="nav-link" href="#contact_me">
-//                   Contact Me
-//                 </a>
-//               </li>
-//               <li className="nav-item">
-//                 <a className="nav-link" href="#resume">
-//                   Resume
-//                 </a>
-//               </li>
-      //       </ul>
-      //     </div>
-      //   </div>
-      // </nav>
-
-//       <div className="content-area">
-//         <div className="content animate__animated animate__fadeIn animate__delay-1s animate__slow centered">
-//           <img id="logo" src={logo} alt="logo" />
-//           <h2>Full-Stack Developer</h2>
-//         </div>
-//       </div>
-//     </header>
-//   );
+  
 }
 
 export default Nav;
